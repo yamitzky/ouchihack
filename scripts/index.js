91b07a12dbd85ddef3e24af5072817de40b38f8f
Yamitzky <negiga@gmail.com>
2018-07-14 22:40:15 +0900

    Add エアコン

--- scripts/index.js
+++ scripts/index.js
@@ -46,18 +46,46 @@ module.exports = async (robot) => {
       Authorization: `Bearer ${process.env.NATURE_REMO_TOKEN}`
     }})
   for (const app of appliances) {
-    for (const sig of app.signals) {
-      robot.hear(new RegExp(`${app.nickname}[ 　のを-]${sig.name}`, 'i'), async (res) => {
+    if (app.aircon) {
+      robot.hear(/(冷房|暖房)[ 　]([\d０-９]+)/i, async (res) => {
         try {
-          await axios.post(`https://api.nature.global/1/signals/${sig.id}/send`, null, {
+          let [_, mode, temp] = res.match
+          mode = {'冷房': 'cool', '暖房': 'warm'}[mode]
+          temp = temp.normalize('NFKC')
+          const { data: { temp: setTemp } } = await axios.post(`https://api.nature.global/1/appliances/${app.id}/aircon_settings`, `temperature=${temp}&operation_mode=${mode}`, {
             headers: {
               Authorization: `Bearer ${process.env.NATURE_REMO_TOKEN}`
             }})
-          res.send(`${app.nickname}-${sig.name} のリモコン操作をしました`)
+          res.send(`エアコンを${setTemp}度に設定しました`)
         } catch (e) {
           res.send(`${e}`)
         }
       })
+      robot.hear(/(?:エアコン|冷房|暖房)[ 　のを-](?:オフ|停止)/i, async (res) => {
+        try {
+          const { data: { button } } = await axios.post(`https://api.nature.global/1/appliances/${app.id}/aircon_settings`, 'button=power-off', {
+            headers: {
+              Authorization: `Bearer ${process.env.NATURE_REMO_TOKEN}`
+            }})
+          res.send(`エアコンをオフにしました(${button})`)
+        } catch (e) {
+          res.send(`${e}`)
+        }
+      })
+    } else {
+      for (const sig of app.signals) {
+        robot.hear(new RegExp(`${app.nickname}[ 　のを-]${sig.name}`, 'i'), async (res) => {
+          try {
+            await axios.post(`https://api.nature.global/1/signals/${sig.id}/send`, null, {
+              headers: {
+                Authorization: `Bearer ${process.env.NATURE_REMO_TOKEN}`
+              }})
+            res.send(`${app.nickname}-${sig.name} のリモコン操作をしました`)
+          } catch (e) {
+            res.send(`${e}`)
+          }
+        })
+      }
     }
   }
 }
